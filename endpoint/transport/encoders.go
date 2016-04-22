f0bafc6cc59ea4d410e47d7bc1442a8231c571ea
mortezaalizadeh <morteza.alizadeh@gmail.com>
2016-04-22 21:55:11 +1200

    Set cross-domain required headers

--- endpoint/transport/encoders.go
+++ endpoint/transport/encoders.go
@@ -7,24 +7,36 @@ import (
 
 func EncodeCreateAddressResponse(w http.ResponseWriter, response interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
 
 	return json.NewEncoder(w).Encode(response)
 }
 
 func EncodeUpdateAddressResponse(w http.ResponseWriter, response interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
 
 	return json.NewEncoder(w).Encode(response)
 }
 
 func EncodeReadAddressResponse(w http.ResponseWriter, response interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
 
 	return json.NewEncoder(w).Encode(response)
 }
 
 func EncodeDeleteAddressResponse(w http.ResponseWriter, response interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
 
 	return json.NewEncoder(w).Encode(response)
 }
