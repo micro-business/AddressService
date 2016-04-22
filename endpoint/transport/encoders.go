ed5bf2f28539c142971026dfb2968e0df28d6d26
mortezaalizadeh <morteza.alizadeh@gmail.com>
2016-04-22 18:56:39 +1200

    Set response content type to be json

--- endpoint/transport/encoders.go
+++ endpoint/transport/encoders.go
@@ -6,17 +6,25 @@ import (
 )
 
 func EncodeCreateAddressResponse(w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
 	return json.NewEncoder(w).Encode(response)
 }
 
 func EncodeUpdateAddressResponse(w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
 	return json.NewEncoder(w).Encode(response)
 }
 
 func EncodeReadAddressResponse(w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
 	return json.NewEncoder(w).Encode(response)
 }
 
 func EncodeDeleteAddressResponse(w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
 	return json.NewEncoder(w).Encode(response)
 }
