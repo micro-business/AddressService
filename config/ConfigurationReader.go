3aadbfaf9602e92383f92ee92784953238c733bc
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-05-11 00:38:52 +1200

    Comment updated, forcing the travis to re-deploy test result using master branch

--- config/ConfigurationReader.go
+++ config/ConfigurationReader.go
@@ -1,6 +1,8 @@
 package config
 
 type ConfigurationReader interface {
+	// GetListeningPort returns the port the application should start listening on.
+	// Returns either the listening port or error if something goes wrong.
 	GetListeningPort() (int, error)
 
 	GetCassandraHosts() ([]string, error)
